f431446ee11fbe532863c05f039952a08c28f50d
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2021-04-03 16:49:05 -0300

    fix API environment variable

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:4000/api/' : process.env.API_URL;
+export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:4000/api/' : process.env.REACT_APP_API_URL;
 
 export default axios.create({
     baseURL: API_URL
